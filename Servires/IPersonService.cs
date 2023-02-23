76bf886c8ec5cc367b7c4a265c96b4f1909e1a06
justin <justinbradleyfields@gmail.com>
2023-02-23 16:42:45 -0500

    Add project files.

---
+++ Servires/IPersonService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorCrud.Models;
+
+namespace BlazorCrud.Servires
+{
+    public interface IPersonService
+    {
+        bool AddUpdate(Person person);
+        bool Delete(int id);
+        Person FindById(int id);
+        List<Person> GetAll();
+    }
+}
